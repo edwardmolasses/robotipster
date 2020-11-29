80e038ce7d7a55bd2e1b194f8b8ef4ccc67f3304
Andrew Lachkovics <andrew.lachkovics@arcteryx.com>
2020-11-28 18:30:20 -0800

    change site other title minor copy change

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,7 +7,7 @@ import Button from "../components/button"
 
 class IndexPage extends React.Component {
   render() {
-    const siteTitle = "Welcome to the robotipster experience"
+    const siteTitle = "Welcome to the Robotipster Experience"
 
     return (
       <Layout location={this.props.location} title={siteTitle}>
